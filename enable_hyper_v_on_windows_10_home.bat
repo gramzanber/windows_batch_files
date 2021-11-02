13788c1f34f37e0951c26a028e07763aa704c07c
Nate Tachibana <gramzanber@gmail.com>
2021-11-02 14:55:50 -0600

    Windows 11 upgrade bypass
    Added a registry file that will let you bypass the CPU and TPM requirements of the Windows 11 upgrade. I also added some comments to both files.

--- enable_hyper_v_on_windows_10_home.bat
+++ enable_hyper_v_on_windows_10_home.bat
@@ -1,6 +1,8 @@
 Rem This script will download the packages that will enable Hyper-V
 Rem This script also works with Windows 11 Home
 Rem You will need to run this script with elevated permissions
+Rem Designed by Nathanial Tachibana
+
 pushd "%~dp0"
 dir /b %SystemRoot%\servicing\Packages\*Hyper-V*.mum >hv.txt
 for /f %%i in ('findstr /i . hv.txt 2^>nul') do dism /online /norestart /add-package:"%SystemRoot%\servicing\Packages\%%i"
